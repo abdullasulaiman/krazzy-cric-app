caf94406c1f1279d326235f22c3dd8faf83c7b57
abdullasulaiman <abdulla.sulaiman@gmail.com>
2019-10-29 20:09:40 +0530

    Initial Push

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+const player = process.argv[2];
+let searchUrl = `https://site.api.espn.com/apis/common/v3/search?sport=cricket&query=${player}&limit=5&type=league%2Cteam%2Cplayer%2Cpersonality%2Cshow%2Cpodcast%2Cradio&mode=prefix&lang=en&region=in&site=espncricinfo&section=cricinfo`
+
 app.get('/', (req, res) => {
+  axios.get(searchUrl)
+  .then( res => {
+    var {
+      items,
+
+    } = res.data;
+    items.map( player => {
+      var {
+        displayName,
+        headshot: {
+          href: playerphoto
+        }
+      } = player;
+      console.log(' Name -> ', displayName);
+      console.log(' Photo -> ', playerphoto);
+    })
+  })
   res.send("This is Krazy API POC ..!");
 })
 
